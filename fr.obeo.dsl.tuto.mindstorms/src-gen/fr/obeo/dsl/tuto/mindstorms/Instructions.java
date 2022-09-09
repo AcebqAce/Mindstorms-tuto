0ad2c34104caad01bdc1c56be1b2f7263d2a67e2
AcebqAce <hadaceplouton@gmail.com>
2022-09-09 16:27:11 +0700

    upload

---
+++ fr.obeo.dsl.tuto.mindstorms/src-gen/fr/obeo/dsl/tuto/mindstorms/Instructions.java
@@ -0,0 +1,18 @@
+/**
+ */
+package fr.obeo.dsl.tuto.mindstorms;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Instructions</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see fr.obeo.dsl.tuto.mindstorms.MindstormsPackage#getInstructions()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Instructions extends EObject {
+} // Instructions
---
+++ fr.obeo.dsl.tuto.mindstorms/src-gen/fr/obeo/dsl/tuto/mindstorms/impl/InstructionsImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package fr.obeo.dsl.tuto.mindstorms.impl;
+
+import fr.obeo.dsl.tuto.mindstorms.Instructions;
+import fr.obeo.dsl.tuto.mindstorms.MindstormsPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Instructions</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class InstructionsImpl extends MinimalEObjectImpl.Container implements Instructions {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected InstructionsImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MindstormsPackage.Literals.INSTRUCTIONS;
+	}
+
+} //InstructionsImpl
